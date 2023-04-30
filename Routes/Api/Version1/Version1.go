49b14a1d305012843ab809c4659c156497f8a865
puthutwahyuaji <puthutwahyuaji13@gmail.com>
2023-04-30 18:49:59 +0700

    grouping

--- Routes/Api/Version1/Version1.go
+++ Routes/Api/Version1/Version1.go
@@ -1,13 +1,12 @@
-package Routes
-
+package Version1
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/initer3737/go-fiber-api/Controllers/HalloController"
 	"github.com/initer3737/go-fiber-api/Controllers/AisatsuController"
 )
 
-func Route__initialize(R * fiber.App){
-	
-	R.Get("/",AisatsuController.Jikoshoukai)
-	R.Get("/hello",HalloController.Hello)
+func Route__ApiV1__initialize(R * fiber.App){
+		v1:=R.Group("/api/v1")
+	v1.Get("/",AisatsuController.Jikoshoukai)
+	v1.Get("/hello",HalloController.Hello)
 }
--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/initer3737/go-fiber-api/Routes"
+	"github.com/initer3737/go-fiber-api/Routes/Api/Version1"
 )
 
 func main() {
     app := fiber.New()
-
-    Routes.Route__initialize(app)
-
+		// version 1 : grouping
+	Version1.Route__ApiV1__initialize(app)
+		//default
+	Routes.Route__initialize(app)
     app.Listen(":3000")
 }
 
